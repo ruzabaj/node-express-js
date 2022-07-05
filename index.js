257f3710bb2138851945af1ef2de1b137916a449
userbaj <64317950+userbaj@users.noreply.github.com>
2022-07-05 18:59:53 +0545

    send data as response using method GET

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
 const app= express();
+
+app.get('/', function(req, res){
+    console.log('GET request');
+    res.send({name:'ruja', age:'23'})
+    res.end();
+})
 app.listen(process.env.port || 4000, function(){
     console.log("lsitening for requests")
 })

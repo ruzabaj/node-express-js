73f96c539346b609776a99f46e4dc54d89181f0c
userbaj <64317950+userbaj@users.noreply.github.com>
2022-07-05 19:03:07 +0545

    send another data to /api route as response using method GET

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.get('/', function(req, res){
     res.send({name:'ruja', age:'23'})
     res.end();
 })
+app.get('/api', function(req, res){
+    console.log('GET request');
+    res.send({name:'ram', age:'33'})
+    res.end();
+})
 app.listen(process.env.port || 4000, function(){
     console.log("lsitening for requests")
 })

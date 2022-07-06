fdb321fd50381fd31f53b795e809eb7381fa4b43
userbaj <64317950+userbaj@users.noreply.github.com>
2022-07-06 14:37:14 +0545

    route changes

---
+++ routes/api.js
@@ -0,0 +1,21 @@
+const express=require('express');
+const router = express.Router();
+
+router.get('/api', function(req,res){
+    res.send({type:'GET'})
+})
+// add
+router.post('/api', function(req,res){
+    res.send({type:'POST'})
+})
+//update
+router.put('/api/:id', function(req,res){
+    res.send({type:'PUT'})
+})
+//delete
+
+router.delete('/api/ninja/:id', function(res,req){
+    res.send({type:'DELETE'})
+})
+
+module.exports= router;
